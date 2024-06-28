38b25a98a12fa323cc37cfb2aff990f736a7e2f3
SDRP8624 <shivdpatel@algomau.ca>
2024-06-28 19:57:09 -0400

    Add files via upload

---
+++ behaviour.php
@@ -0,0 +1,9 @@
+< DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> ‹title>Header</title>
+</head> <body>
+<h1> Here is the description of the behaviour of the Animal</h1>
+</body>
+</html>
